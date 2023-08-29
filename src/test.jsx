286a0960358d1f923c59089b2408b8a85a4bef7d
Dmytro Haidai <dimasow12@gmail.com>
2023-08-29 20:38:41 +0300

    Update test.jsx

--- src/test.jsx
+++
@@ -1,9 +0,0 @@
-evt.preventDefault();
-const searchValue = evt.target[0].value.trim();
-if (!searchValue) {
-  Notiflix.Notify.info(
-    'The search bar cannot be empty. Please type any criteria in the search bar.'
-  );
-} else {
-  setSearchParams({ search: searchValue, page: 1 });
-}
